31e0e8996d7e4f8407e0fb64b0f609f1da02c164
Eduardo Campos de Souza <eduardofidera@gmail.com>
2021-01-13 17:26:11 -0300

    Change welcome message

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://herz-ui.micromed.io/">Micromed!</a>
+          Welcome to <a href="https://herz-ui.micromed.io/">Herz UI!</a>
         </h1>
 
         <p className={styles.description}>
